c31e74adc81f3fed3a61980b038a5e4abc8c8de6
ggquicky <ggquicky@gmail.com>
2022-09-07 15:29:46 -0600

    remove unnnecesary files and add tailwind css

---
+++ src/components/navbar/NavBar.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+let id = 0;
+const navBarItems = [
+  { id: id++, name: "Home", value: "/" },
+  { id: id++, name: "Explore", value: "/explore" },
+  { id: id++, name: "Activity", value: "/activiy" },
+  { id: id++, name: "Comunity", value: "/comunity;" },
+  { id: id++, name: "About", value: "/about" },
+];
+</script>
+
+<template>
+  <div class="flex items-center justify-evenly">
+    <img alt="Vue logo" height="40" width="40" src="@/assets/logo.svg" />
+    <div class="flex items-center justify-center">
+      <RouterLink
+        v-for="navBarItem in navBarItems"
+        :key="navBarItem"
+        :to="navBarItem.value"
+        >{{ navBarItem.name }}</RouterLink
+      >
+    </div>
+    <div class="font-bold">Search</div>
+  </div>
+</template>
